145afc9895e25c8f3a779cdc5f801a5b1f1292da
Marcelo Cordeiro De Quadros <marcelo.quadros@livelo.com.br>
2018-12-15 13:07:20 -0200

    Init project

---
+++ src/main/java/br/com/comexport/javachallenge/JavaChallengeApplication.java
@@ -0,0 +1,26 @@
+package br.com.comexport.javachallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
+
+@SpringBootApplication
+public class JavaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaChallengeApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.getDefault());
+		return localeResolver;
+	}
+
+}
+
